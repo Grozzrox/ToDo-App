1448182fa2b19ef7c20fa58368470a5ffd87dc33
Andrew <grozzy09@gmail.com>
2020-12-16 11:42:07 -0500

    16Dec2020_1

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const todoList = document.querySelector('#todo-list');
 const form = document.querySelector('#todo');
 const input = document.querySelector('input[type="text"]');
+const saved = localStorage.getItem("todoList");
+
+if (saved) {
+    todoList.innerHTML = saved;
+}
 
 todoList.addEventListener('click', function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("completed");
+        localStorage.setItem("todoList", todoList.innerHTML);
     } else if (e.target.tagName === "BUTTON") {
         e.target.parentElement.remove();
+        localStorage.setItem("todoList", todoList.innerHTML);
     }
 })
 
@@ -18,4 +25,5 @@ form.addEventListener('submit', function(e) {
     newItem.innerText = `${input.value} `;
     newItem.appendChild(newBtn);
     todoList.appendChild(newItem);
+    localStorage.setItem("todoList", todoList.innerHTML);
 })
